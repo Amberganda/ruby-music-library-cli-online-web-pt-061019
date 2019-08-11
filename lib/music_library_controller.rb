faf6f0e68d4d68e84f74696e69718b9a9e943d85
Amber Taranto <ambzzzz@gmail.com>
2019-08-11 21:44:05 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,10 +2,10 @@ class MusicLibraryController
 
     # attr_reader :path
 
-    def initialize(path)
+    def initialize(path = "./db/mp3s")
         # @path = path
         @importer = MusicImporter.new(path)
-        
+
     end
 
 end
